6ad212dba093e9e43a9cb27f6ca2940d23baf9d3
Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>
2026-07-03 18:09:36 -0300

    feat: Instance attributes
    Signed-off-by: Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>

--- modelos/restaurant.py
+++ modelos/restaurant.py
@@ -1,9 +1,18 @@
+import os
+
 class  Restaurant:
     name =''
     category = ''
     active = False
 
-Restaurant_square = Restaurant( )
+Restaurant_square = Restaurant()
+Restaurant_square.name = 'Square'
+Restaurant_square.category = 'Gourmet'
+
 Restaurant_pizza = Restaurant()
 
 Restaurant = [Restaurant_square, Restaurant_pizza]
+
+print(vars(Restaurant_square))
+os.system('cls')
+print(vars(Restaurant_square))
