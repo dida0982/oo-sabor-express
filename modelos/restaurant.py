0c8b87743f3a359efe3b2030534fb007032b43f8
Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>
2026-07-03 17:07:39 -0300

    feat: add file restaurant
    Signed-off-by: Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>

---
+++ modelos/restaurant.py
@@ -0,0 +1,9 @@
+class  Restaurant:
+    name =''
+    category = ''
+    active = False
+
+Restaurant_square = Restaurant( )
+Restaurant_pizza = Restaurant()
+
+Restaurant = [Restaurant_square, Restaurant_pizza]
