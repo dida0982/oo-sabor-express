01986524461423c074cc73eb27d13a53b4421ba9
Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>
2026-07-07 06:08:02 -0300

    feat: Restaurant list method created.
    Signed-off-by: Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>

--- modelos/restaurant.py
+++ modelos/restaurant.py
@@ -1,18 +1,21 @@
 import os
-
 class  Restaurant:
+    restaurants = []
+
     def __init__(self,name,category):
         self.name = name
         self.category = category
         self.active = False
+        Restaurant.restaurants.append((self))
+
     def __str__(self):
         return f'{self.name} | {self.category}'
+
+    def restaurants_list():
+        for restaurant in Restaurant.restaurants:
+            print(f'{restaurant.name} | {restaurant.category} | {restaurant.active}')
 
 restaurant_square = Restaurant('Square','Gourmet' )
 restaurant_pizza = Restaurant('Pizza Express', 'Italiana')
 
-Restaurant = [restaurant_square, restaurant_pizza]
-
-os.system('cls')
-print(restaurant_square)
-print(restaurant_pizza)
+Restaurant.restaurants_list()
