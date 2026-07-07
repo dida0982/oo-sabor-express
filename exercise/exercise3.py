a34de8580489c3740f0fd6c665f489af7f9782d8
Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>
2026-07-07 06:17:45 -0300

    feat: completed exercises.
    Signed-off-by: Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>

---
+++ exercise/exercise3.py
@@ -0,0 +1,8 @@
+'''
+Exercícios
+Implemente uma classe chamada Carro com os atributos básicos, como modelo, cor e ano. Crie uma instância dessa classe e atribua valores aos seus atributos.
+Crie uma classe chamada Restaurante com os atributos nome, categoria, ativo e crie mais 2 atributos. Instancie um restaurante e atribua valores aos seus atributos.
+Modifique a classe Restaurante adicionando um construtor que aceita nome e categoria como parâmetros e inicia ativo como False por padrão. Crie uma instância utilizando o construtor.
+Adicione um método especial __str__ à classe Restaurante para que, ao imprimir uma instância, seja exibida uma mensagem formatada com o nome e a categoria. Exiba essa mensagem para uma instância de restaurante.
+Crie uma classe chamada Cliente e pense em 4 atributos. Em seguida, instancie 3 objetos desta classe e atribua valores aos seus atributos através de um método construtor.
+'''
