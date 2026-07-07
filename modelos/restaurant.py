cb31e29e9df82b85f7609635a975a9f9f6d0c548
Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>
2026-07-07 12:34:53 -0300

    feat: property completd
    Signed-off-by: Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>

--- modelos/restaurant.py
+++ modelos/restaurant.py
@@ -5,15 +5,20 @@ class  Restaurant:
     def __init__(self,name,category):
         self.name = name
         self.category = category
-        self.active = False
+        self._active = False
         Restaurant.restaurants.append((self))
 
     def __str__(self):
         return f'{self.name} | {self.category}'
 
     def restaurants_list():
+        print(f'{'Name of restaurant.ljust(25)'} | {'Category.ljust(25)'} | {'Status'}')
         for restaurant in Restaurant.restaurants:
-            print(f'{restaurant.name} | {restaurant.category} | {restaurant.active}')
+            print(f'{restaurant.name.ljust(25)} | {restaurant.category.ljust(25)} | {restaurant.active}')
+
+    @property
+    def active(self):
+        return '⌧' if self._active else '☐'
 
 restaurant_square = Restaurant('Square','Gourmet' )
 restaurant_pizza = Restaurant('Pizza Express', 'Italiana')
