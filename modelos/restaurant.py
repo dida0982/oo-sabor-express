65c71c330da272c5c35b83e59953997d6a4a53d7
Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>
2026-07-07 20:17:21 -0300

    feat: applied class methods
    Signed-off-by: Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>

--- modelos/restaurant.py
+++ modelos/restaurant.py
@@ -3,24 +3,32 @@ class  Restaurant:
     restaurants = []
 
     def __init__(self,name,category):
-        self.name = name
-        self.category = category
+        #title() deixa todas as primeiras letras em maiusculas.
+        self._name = name.title()
+        #upper() deixa todas as primeiras letras em maiusculas.
+        self._category = category.upper()
         self._active = False
-        Restaurant.restaurants.append((self))
+        Restaurant.restaurants.append(self)
 
     def __str__(self):
-        return f'{self.name} | {self.category}'
+        return f'{self._name} | {self._category}'
 
-    def restaurants_list():
-        print(f'{'Name of restaurant.ljust(25)'} | {'Category.ljust(25)'} | {'Status'}')
-        for restaurant in Restaurant.restaurants:
-            print(f'{restaurant.name.ljust(25)} | {restaurant.category.ljust(25)} | {restaurant.active}')
+    @classmethod
+    def restaurants_list(cls):
+        print(f'{'Name of restaurant'.ljust(25)} | {'Category'.ljust(25)} | {'Status'}')
+        for restaurant in cls.restaurants:
+            print(f'{restaurant._name.ljust(25)} | {restaurant._category.ljust(25)} | {restaurant.active}')
 
     @property
     def active(self):
-        return '⌧' if self._active else '☐'
+        return '✔️' if self._active else '❌'
+
+    def toggle_state(self):
+        self._active = not self._active
 
 restaurant_square = Restaurant('Square','Gourmet' )
+restaurant_square.toggle_state()
 restaurant_pizza = Restaurant('Pizza Express', 'Italiana')
 
+os.system('cls')
 Restaurant.restaurants_list()
